6a2f5d35cf36af2799106b4045d902a37ac66b01
Pawel Pabich <pawel@pabich.eu>
2015-07-17 14:05:08 +1000

    props done

--- build/all.js
+++ build/all.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
 
-React.render(React.createElement(
-    'h1',
-    null,
-    'Hello, world!'
-), document.getElementById('example'));
+var Child = React.createClass({
+    displayName: "Child",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "commentBox" },
+            "I am: ",
+            this.props.firstName
+        );
+    }
+});
+
+var Parent = React.createClass({
+    displayName: "Parent",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(Child, { firstName: "Child1" }),
+            React.createElement(Child, { firstName: "Child3" })
+        );
+    }
+});
+
+React.render(React.createElement(Parent, null), document.getElementById("example"));
 //# sourceMappingURL=all.js.map
--- src/test.jsx
+++ src/test.jsx
@@ -1,4 +1,25 @@
+var Child = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+               I am: {this.props.firstName}
+            </div>
+        );
+    }
+});
+
+var Parent = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Child firstName="Child1" />
+                <Child firstName="Child3" />
+            </div>
+        );
+    }
+});
+
 React.render(
-    <h1>Hello, world!</h1>,
+    <Parent />,
     document.getElementById('example')
 );
